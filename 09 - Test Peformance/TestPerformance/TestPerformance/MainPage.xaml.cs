7672bd752be7bdd4ecbe585034936fa5d3d5205f
Tuan Tran Van <tuantran1708@gmail.com>
2016-03-13 17:22:35 +0700

    first test

--- 09 - Test Peformance/TestPerformance/TestPerformance/MainPage.xaml.cs
+++ 09 - Test Peformance/TestPerformance/TestPerformance/MainPage.xaml.cs
@@ -1,30 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using System.Diagnostics;
 
 namespace TestPerformance
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Debug.WriteLine("Start the test");
+            //Test the string concat
+            for (int i = 0; i < 100000; i++)
+            {
+                //string s = "Tuan Tran Van";
+                //s += " welcome to my test";
+                //s += " and hello world";
+                //s += i;
+            }
+            Debug.WriteLine("Test end");
         }
     }
 }
